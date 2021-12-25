af9986401b17c3eb12115cf9f8177d023a2fd9ff
Aaqib Javed <ajmehdi5@gmail.com>
2021-12-25 23:26:33 +0500

    request users api

--- src/view/auth/Login.jsx
+++ src/view/auth/Login.jsx
@@ -5,27 +5,19 @@ const Login = () => {
     const [password,setPassword] = useState('');
     const [user_name,setUserName] = useState('');
 
-
-    const newPost = {
-    user_name: user_name,
-    email: email,
-    password: password
-};
-
 const sendPostRequest = async (e) => {
     e.preventDefault();
-    try {
-        const resp = await axios.post('http://localhost:3001/users/register',{
-        newPost,
-        headers: {
-          "x-apikey": "API_KEY",},
-        responseType: "json",
-      });
-        console.log(resp.data);
-    } catch (err) {
-        // Handle Error Here
-        console.error(err);
-    }
+    axios.post('/users/register', {
+    "user_name": user_name,
+    "email": email,
+    "password": password
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 };
 
 
@@ -47,7 +39,7 @@ const sendPostRequest = async (e) => {
 
 
     return ( <div>
-        <form >
+        <form action="" method="POST">
             <input placeholder="User Name" name = "user_name"
             value={user_name}
             onChange={(e)=>{setUserName(e.target.value)}}
